85ebdb67b7540209db338c9b7b8bf7f44795c077
Solanki777 <solankimaheshkhash7@gmail.com>
2026-09-22 22:49:18 +0530

    things are going interesting : just price added

--- apna_cart/src/components/Product.jsx
+++ apna_cart/src/components/Product.jsx
@@ -2,20 +2,37 @@ import React from "react";
 
 export default function Product(props) {
   return (
-    <div>
-      <h2> Item Name : {props.product.name}</h2>
-      <h2>
-        {" "}
-        Price :<span class="badge bg-secondary">₹{props.product.price} </span>
-      </h2>
-      <button type="button" class="btn btn-primary btn-sm">
-        Small button
-      </button>
-      <button type="button" class="btn btn-secondary btn-sm">
-        Small button
-      </button>
+    <div className = "row">
+      <div className="col-8">
+        <div>
+          <h2> Item Name : {props.product.name}</h2>
+          <h2>
+            {" "}
+            Price :
+            <span class="badge bg-secondary">₹{props.product.price} </span>
+          </h2>
+        </div>
+        <div>
+          <button type="button" class="btn btn-primary btn-sm">
+            +
+          </button>
 
-      <hr></hr>
+          <h2 class="btn sm">{props.product.quantity}</h2>
+
+          <button type="button" class="btn btn-secondary btn-sm">
+            {" "}
+            -
+          </button>
+        </div>
+
+        <hr></hr>
+      </div>
+
+
+      <div className = "col-4">
+        {props.product.quantity * props.product.price}
+
+      </div>
     </div>
   );
 }
