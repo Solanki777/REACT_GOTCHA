766947bbe30db60bba4283be7271626dfc06554a
Solanki777 <solankimaheshkhash7@gmail.com>
2026-09-23 23:57:27 +0530

    button added and giving better ui

--- apna_cart/src/components/Footer.jsx
+++ apna_cart/src/components/Footer.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='row fixed-bottom'>
+        <div className='col-8'>
+            <button className='btn btn-primary'>
+                Reset
+            </button>
+        </div>
+        <div className='col-2 bg-dark'>
+            here the values are displyed
+            
+        </div>
+
+        <div className='col-2'>
+            <button className='btn btn-danger'>
+                PayNow
+            </button>
+        </div>
+    </div>
   )
 }
