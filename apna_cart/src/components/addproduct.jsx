187c2fca416903b21e839e025f67cdf0ef28f32f
Solanki777 <solankimaheshkhash7@gmail.com>
2026-09-25 23:23:11 +0530

    basic form is ready

--- apna_cart/src/components/addproduct.jsx
+++ apna_cart/src/components/addproduct.jsx
@@ -3,13 +3,23 @@ import React from 'react'
 export default function addproduct() {
   return (
     <div className='row'>
-        item name
-        <div></div>
-        item price
-        <div></div>
-        add 
-        <div>
+        
+        <div className = 'col-8'>
           
+          <input type="text" className="" placeholder='item name' />
+
+        </div>
+        
+        <div className="col-2">
+          
+          <input type="number" className="" placeholder='item price' />
+
+        </div>
+        
+        <div className="col-2">
+          <button className="btn btn-danger">
+            add
+          </button> 
         </div>
       </div>
   )
